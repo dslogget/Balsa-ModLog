c76b0517ff768fe0a10b70672a07c0f9965e4b57
Daniel Sloggett <48067784+dslogget@users.noreply.github.com>
2021-09-26 20:59:29 +0100

    Big overhaul to make levels be a thing

---
+++ ModLog/Loader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using BalsaCore;
+
+namespace DarkLog
+{
+    [BalsaAddon]
+    public class Loader
+    {
+        private static bool loaded = false;
+        private static GameObject go;
+        private static MonoBehaviour mod;
+
+        [BalsaAddonInit]
+        public static void BalsaInit()
+        {
+
+            if (!loaded)
+            {
+                loaded = true;
+                go = new GameObject();
+            }
+            mod = go.AddComponent<ModLog>();
+        }
+
+        [BalsaAddonInit(invokeTime = AddonInvokeTime.Flight)]
+        public static void BalsaInitFlight()
+        {
+
+        }
+
+        [BalsaAddonFinalize(invokeTime = AddonInvokeTime.Flight)]
+        public static void BalsaFinalizeFlight()
+        {
+
+        }
+        //Game exit
+        [BalsaAddonFinalize]
+        public static void BalsaFinalize()
+        {
+            go.DestroyGameObject();
+        }
+
+    }
+}
--- ModLog/Log.cs
+++ ModLog/Log.cs
@@ -1,28 +1,169 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Collections;
+using UnityEngine;
 
 namespace DarkLog
 {
-    public class ModLog
+    public class ModLog : UnityEngine.MonoBehaviour
     {
-        private string modName;
-        private long realtimeEpoch;
-        private ConcurrentQueue<string> messages = new ConcurrentQueue<string>();
+        private static long realtimeEpoch;
+        private readonly static ConcurrentQueue<string> errorMessages = new ConcurrentQueue<string>();
+        private readonly static ConcurrentQueue<string> warningMessages = new ConcurrentQueue<string>();
+        private readonly static ConcurrentQueue<string> infoMessages = new ConcurrentQueue<string>();
+        private readonly static Dictionary<string, int> handleLevel = new Dictionary<string, int>();
+        public const int MaxLogLevel = sizeof(int) * 8 - 1;
+        public const int ErrorLevels = 5;
+        public const int WarningLevels = 5;
 
-        public ModLog(string modName)
+        static ModLog()
         {
+            realtimeEpoch = 0;
+            SetHandleLevel("ModLog", new int[] {  
+                0, 
+                1, 
+                2 }, true);
+            SetHandleLevelW("ModLog", new int[] {
+                0,
+                1,
+                2 }, true);
+            SetHandleLevelE("ModLog", new int[] {
+                0,
+                1,
+                2 }, true);
+
+            Chat.GameChat.OnPlayerMessageReceived.AddListener(HandleMessage);
+        }
+
+        private static void HandleMessage( Chat.ChatData chatData )
+        {
+            bool success = false;
+            if ( chatData.player.isLocalPlayer && chatData.isCommand )
+            {
+                Regex ModLogrx = new Regex(@"^/ModLog", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                if (ModLogrx.IsMatch(chatData.message) )
+                {
+                    Regex setLevelRx = new Regex(@"^\/ModLog\s+(?<op>s|c)[a-z]*\s+(?<handle>[a-z]+)\s+l[a-z]*\s+(?<type>[iwe])[a-z]*\s+(?<levels>(?:[0-9]+)(?:\s+[0-9]+)*)\s*", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                    Match match = setLevelRx.Match(chatData.message);
+                    if (match.Success)
+                    {
+                        bool set = match.Groups["op"].Value.ToLower()[0] == 's';
+                        string handle = match.Groups["handle"].Value.ToLower();
+                        string type = match.Groups["type"].Value.ToLower();
+                        string[] levelsStr = match.Groups["levels"].Value.Split(' ');
+                        int[] levels = new int[levelsStr.Length];
+                        for (int i = 0; i < levels.Length; i++)
+                        {
+                            levels[i] = int.Parse(levelsStr[i]);
+                        }
+                        switch (type[0]) {
+                            case 'i':
+                                ModLog.SetHandleLevel(handle, levels, set) ;
+                                success = true;
+                                break;
+                            case 'w':
+                                ModLog.SetHandleLevelW(handle, levels, set);
+                                success = true;
+                                break;
+                            case 'e':
+                                ModLog.SetHandleLevelE(handle, levels, set);
+                                success = true;
+                                break;
+                            default:
+                                break;
+                        }
+                    } 
+                    else
+                    {
+                        LogThread("ModLog", 4, "TestI");
+                        LogThreadW("ModLog", 4, "TestW");
+                        LogThreadE("ModLog", 4, "TestE");
+                    }
+                    if (!success)
+                    {
+                        Chat.GameChat.PostServerMessage("InvalidCommand, usage: \"/ModLog set/clear <handle> level info/warning/error <level1> <level2> ...\"");
+                        LogThread("ModLog", 0, "Invalid Command");
+                    }
+                }
+            }
+        }
+
+
+        public void Awake()
+        {
+            GameObject.DontDestroyOnLoad(this);
             realtimeEpoch = DateTime.UtcNow.Ticks - (long)(UnityEngine.Time.realtimeSinceStartup * TimeSpan.TicksPerSecond);
-            this.modName = modName;
+            Log("ModLog", 0, "SanityInfo");
+            LogW("ModLog", 0, "SanityWarning");
+            LogE("ModLog", 0, "SanityError");
         }
 
-        public void Log(string message)
+        public static int ELevel(int i)
         {
-            UnityEngine.Debug.Log(PrependText(message));
+            return i;
         }
 
-        public void LogThread(string message)
+        public static int WLevel(int i)
         {
-            messages.Enqueue(PrependText(message));
+            return i + ErrorLevels;
+        }
+
+        public static int ILevel(int i)
+        {
+            return i + ErrorLevels + WarningLevels;
+        }
+
+        private static bool LevelIsActive( string handle, int logLevel )
+        {
+            return handleLevel.TryGetValue(handle, out int levels) && (levels & (1 << logLevel)) != 0;
+        }
+
+        public static void Log(string handle, int logLevel, string message)
+        {
+            if (LevelIsActive(handle.ToLower(), ILevel(logLevel)))
+            {
+                    UnityEngine.Debug.Log(PrependText(logLevel, handle, message));
+            }
+        }
+        public static void LogW(string handle, int logLevel, string message)
+        {
+            if (LevelIsActive(handle.ToLower(), WLevel(logLevel)))
+            {
+                UnityEngine.Debug.LogWarning(PrependText(logLevel, handle, message));
+            }
+        }
+
+        public static void LogE(string handle, int logLevel, string message)
+        {
+            if (LevelIsActive(handle.ToLower(), ELevel(logLevel)))
+            {
+                UnityEngine.Debug.LogError(PrependText(logLevel, handle, message));
+            }
+        }
+
+        public static void LogThread(string handle, int logLevel, string message)
+        {
+            if (LevelIsActive(handle.ToLower(), ILevel(logLevel)))
+            {
+                    infoMessages.Enqueue(PrependText(logLevel, handle, message));
+            }
+        }
+
+        public static void LogThreadW(string handle, int logLevel, string message)
+        {
+            if (LevelIsActive(handle.ToLower(), WLevel(logLevel)))
+            {
+                warningMessages.Enqueue(PrependText(logLevel, handle, message));
+            }
+        }
+        public static void LogThreadE(string handle, int logLevel, string message)
+        {
+            if (LevelIsActive(handle.ToLower(), ELevel(logLevel)))
+            {
+                errorMessages.Enqueue(PrependText(logLevel, handle, message));
+            }
         }
 
         /// <summary>
@@ -30,21 +171,119 @@ namespace DarkLog
         /// </summary>
         public void Update()
         {
-            while (messages.TryDequeue(out string message))
+            while (errorMessages.TryDequeue(out string message))
+            {
+                UnityEngine.Debug.LogError(message);
+            }
+
+            while (warningMessages.TryDequeue(out string message))
+            {
+                UnityEngine.Debug.LogWarning(message);
+            }
+
+            while (infoMessages.TryDequeue(out string message))
             {
                 UnityEngine.Debug.Log(message);
             }
         }
 
-        private string PrependText(string message)
+        private static void SetHandleLevel(string handle, int level, bool enable)
         {
-            return $"{GetTime()} [{modName}] {message}";
+            SetHandleLevelGeneric(handle, ILevel(level), enable);
+        }
+        private static void SetHandleLevelW(string handle, int level, bool enable)
+        {
+            SetHandleLevelGeneric(handle, WLevel(level), enable);
+        }
+
+        private static void SetHandleLevelE(string handle, int level, bool enable)
+        {
+            SetHandleLevelGeneric(handle, ELevel(level), enable);
+        }
+
+        private static void SetHandleLevelGeneric(string handle, int level, bool enable)
+        {
+            handle = handle.ToLower();
+            if ( level < 0 || level > MaxLogLevel)
+            {
+                throw new ArgumentOutOfRangeException("level", $"level must be between 0 and {MaxLogLevel - WarningLevels - ErrorLevels}");
+            }
+            if ( !handleLevel.ContainsKey(handle) )
+            {
+                handleLevel.Add(handle, 0);
+            }
+
+            if (enable)
+            {
+                handleLevel[handle] |= (int)(1 << level);
+            }
+            else
+            {
+                handleLevel[handle] &= (int)~(1 << level);
+            }
+        }
+
+
+        private static void SetHandleLevel(string handle, int[] levels, bool enable)
+        {
+            foreach ( int level in levels )
+            {
+                SetHandleLevelGeneric(handle, ILevel(level), enable);
+            }
+        }
+        private static void SetHandleLevelW(string handle, int[] levels, bool enable)
+        {
+            foreach (int level in levels)
+            {
+                SetHandleLevelGeneric(handle, WLevel(level), enable);
+            }
+        }
+
+        private static void SetHandleLevelE(string handle, int[] levels, bool enable)
+        {
+            foreach (int level in levels)
+            {
+                SetHandleLevelGeneric(handle, ELevel(level), enable);
+            }
+        }
+
+        private static void SetHandleLevelGeneric(string handle, int[] levels, bool enable)
+        {
+            handle = handle.ToLower();
+            foreach (int level in levels ) {
+                if (level < 0 || level > MaxLogLevel)
+                {
+                    throw new ArgumentOutOfRangeException("level", $"level must be between 0 and {MaxLogLevel - WarningLevels - ErrorLevels}");
+                }
+                if (!handleLevel.ContainsKey(handle))
+                {
+                    handleLevel.Add(handle, 0);
+                }
+
+                if (enable)
+                {
+                    handleLevel[handle] |= (int)(1 << level);
+                }
+                else
+                {
+                    handleLevel[handle] &= (int)~(1 << level);
+                }
+            }
+        }
+
+        private static string PrependText(int level, string handle, string message)
+        {
+            return $"{GetTime()} [{handle}] {{{level}}} {message}";
         }
 
         //RealTimeSinceStartup is not thread safe... somehow.
-        private float GetTime()
+        private static float GetTime()
         {
             return (DateTime.UtcNow.Ticks - realtimeEpoch) / (float)TimeSpan.TicksPerSecond;
         }
+
+
+
+
     }
 }
